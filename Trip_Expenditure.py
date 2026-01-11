e052a34bc80789a682d3d63bdb026ea725d82f0a
chahak1707 <pahujamanni1978@gmail.com>
2026-01-11 14:42:08 +0530

    Module3-lesson6

---
+++ Trip_Expenditure.py
@@ -0,0 +1,6 @@
+travel_cost= float(input("Enter The Travel Cost:"))
+food_cost= float(input("Enter The Food Cost:"))
+stay_cost= float(input("Enter The Stay Cost:"))
+
+total_expenditure= travel_cost+food_cost+stay_cost
+print("Total Expenditure:",total_expenditure)
