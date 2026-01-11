1735ca94aa252c379e8834cdbd232d4ac14c2d94
chahak1707 <pahujamanni1978@gmail.com>
2026-01-11 14:33:21 +0530

    Modue3-lesson6

---
+++ DateTime_Module.py
@@ -0,0 +1,6 @@
+import datetime
+current_datetime= datetime.datetime.now()
+print("Current date and time:",current_datetime)
+
+current_datetime= datetime.datetime.today().date()
+print("Today is",current_datetime)
